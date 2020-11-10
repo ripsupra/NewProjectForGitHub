c14710c632368c3a5f8d4bfc4f3e3feb60a823fe
Adil Imanaliev <imanaliev.adil@gmail.com>
2020-11-10 14:48:44 -0500

    More codes added

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // Akbar < Muhtar
         System.out.println("<Muhtar"+"Akbar");
+        System.out.println("fuck");
     }
 }
